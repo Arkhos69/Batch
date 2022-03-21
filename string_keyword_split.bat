cd9a4e0836374cc70a29d3be1f31cbf06283c415
Arkhos69 <98234168+Arkhos69@users.noreply.github.com>
2022-03-21 23:55:40 +0800

    Update string_keyword_split.bat

--- string_keyword_split.bat
+++ string_keyword_split.bat
@@ -1,21 +1,22 @@
 @echo off &setlocal EnableDelayedExpansion
 
-echo Keyword=cut
-echo String=123cut456cut789
+set "string=123\n456\n789"
+echo Keyword=\n
+echo String=123\n456\n789
 echo Return:
-call :str_keyword_split return "123cut456cut789" "cut"
+call :str_keyword_split return string "\n"
 if %errorlevel%==0 for /l %%0 in (0, 1, %sp_ary_len%) do echo !return[%%0]! &set "return[%%0]="
 
 :main
 echo.
 if defined keyword (echo [Keyword=^%keyword%]) else set /p "keyword=Set Keyword:"
 set /p "enter=Enter Split String:"
-if defined enter call :str_keyword_split return "%enter%" "%keyword%" & ^
-if %errorlevel%==0 for /l %%0 in (0, 1, !sp_ary_len!) do echo !return[%%0]! &set "return[%%0]="
+if defined enter call :str_keyword_split return enter "%keyword%" & ^
+if !errorlevel!==0 for /l %%0 in (0, 1, !sp_ary_len!) do echo !return[%%0]! &set "return[%%0]="
 set "enter=" &goto main
 
-:str_keyword_split <name_ReturnArray> <string_Str> <string_KeyWord>
-set "sp_return=%~1" &set "sp_str=%~2" &set "sp_Keyword=%~3"
+:str_keyword_split <name_ReturnArray> <name_String> <string_KeyWord>
+set "sp_return=%~1" &set "sp_str=!%~2!" &set "sp_Keyword=%~3"
 for %%a in (sp_return sp_str sp_Keyword) do if not defined %%a exit /b 1
 set "sp_get=" &set "sp_tmp=" &set /a len_=0 &set /a sp_ary_len=-1
 :str_keyword_split_loop
