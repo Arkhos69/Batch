bd5e4d12e796a165604bd063c1f35fa8cd0848db
Arkhos69 <98234168+Arkhos69@users.noreply.github.com>
2022-03-24 10:42:40 +0800

    Create sync_control.bat

---
+++ sync_control.bat
@@ -0,0 +1,21 @@
+@echo off &setlocal EnableDelayedExpansion
+color 0a
+
+if exist switch.sync del switch.sync
+if defined pass (title=__sync__catch__ &goto loop) ^
+
+else (title=__sync__input__ &set "choice_=1 2 3 4 5 w a s d"
+for %%a in (!choice_!) do set /a len+=1 &set "input[!len!]=%%a"
+set "choice_=!choice_: =!"
+set "pass=1" &start %~f0)
+
+:main
+choice /n /c %choice_% /m "%choice_%:"
+for /l %%0 in (1, 1, %len%) do if !errorlevel!==%%0 echo !input[%%0]! >switch.sync
+goto main
+
+:loop
+if exist switch.sync (
+for /f "tokens=*" %%a in ('type switch.sync') do echo %%a
+del switch.sync)
+goto loop
